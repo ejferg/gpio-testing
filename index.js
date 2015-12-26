7f836111f6331f69197186bdbb6e950bad1487b1
Earl Ferguson <earl@ullisen.com>
2015-12-26 02:51:01 -0500

    changes

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var gpio = require("pi-gpio");
 
-var gpio16 = gpio.open(16, 'in up', function(err) {
+gpio.open(16, 'in up', function(err) {
 
   var read = function() {
-    gpio16.read(16, function(err, value) {
+    gpio.read(16, function(err, value) {
       console.error('Error: ', error);
       console.log('Value: ', value);
 
