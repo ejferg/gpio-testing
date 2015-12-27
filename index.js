5a4ca216a4b6b4f7f486810684593a2c2f9d12a6
Earl Ferguson <earl@ullisen.com>
2015-12-27 12:28:58 -0500

    changes

--- index.js
+++ index.js
@@ -1,27 +1,43 @@
 var gpio = require("pi-gpio");
 
-var pin = 16;
+var pin = 7;
 
-gpio.close(pin);
-gpio.open(pin, 'in', function(err) {
+gpio.open(pin, "output", function(err) {
+  console.log(err);
+  gpio.write(pin, 1, function() {
+    console.log('Write Complete');
+  });
 
-  var read = function() {
-    gpio.read(pin, function(err, value) {
-
-      if(err) {
-        console.error('Error: ', err);
-        gpio.close(pin);
-
-        return;
-      }
-
-      console.log('Value: ', value);
-
-      setTimeout(function(){
-        read();
-      }, 100);
+  setTimeout(function() {
+    console.log('Stop');
+    gpio.write(pin, 0, function() {
+      gpio.close(pin);
     });
-  };
 
-  read();
+  }, 2000);
+
 });
+
+// gpio.close(pin);
+// gpio.open(pin, 'in', function(err) {
+//
+//   var read = function() {
+//     gpio.read(pin, function(err, value) {
+//
+//       if(err) {
+//         console.error('Error: ', err);
+//         gpio.close(pin);
+//
+//         return;
+//       }
+//
+//       console.log('Value: ', value);
+//
+//       setTimeout(function(){
+//         read();
+//       }, 100);
+//     });
+//   };
+//
+//   read();
+// });
